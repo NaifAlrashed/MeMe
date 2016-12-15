0ac2b4b5dd61a0fd8ab2b6f9292263cc3d54ec37
naifalrashed <naialrashed@alfaisal.edu>
2016-12-15 20:00:11 +0300

    add top and bottom tool-bars and imageView. also creat the functionality for the camera and the album

--- MeMe/ViewController.swift
+++ MeMe/ViewController.swift
@@ -8,18 +8,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var cameraButton: UIBarButtonItem!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewWillAppear(_ animated: Bool) {
+        cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
     }
 
+    @IBAction func camera(_ sender: Any) {
+        createAnLaunchUIImagePicker(with: .camera)
+    }
 
+    @IBAction func album(_ sender: Any) {
+        createAnLaunchUIImagePicker(with: .photoLibrary)
+    }
+    
+    private func createAnLaunchUIImagePicker(with type: UIImagePickerControllerSourceType) {
+        let controller = UIImagePickerController()
+        controller.delegate = self
+        controller.sourceType = type
+        present(controller, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let image = info["UIImagePickerControllerOriginalImage"] as? UIImage {
+            imageView.image = image
+        }
+        
+        dismiss(animated: true, completion: nil)
+    }
 }
 
