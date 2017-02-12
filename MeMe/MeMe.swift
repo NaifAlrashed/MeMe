8581028110b814739f4f47aab441c6d96d03d2ab
naifalrashed <naialrashed@alfaisal.edu>
2017-02-12 10:02:53 +0300

    change model from class to struct

--- MeMe/MeMe.swift
+++ MeMe/MeMe.swift
@@ -8,16 +8,9 @@
 
 import UIKit
 
-class MeMe {
+struct MeMe {
     let topText: String
     let bottomText: String
     let originalImage: UIImage
     let memedImage: UIImage
-    
-    init(topText: String, bottomText: String, originalImage: UIImage, memedImage: UIImage) {
-        self.topText = topText
-        self.bottomText = bottomText
-        self.originalImage = originalImage
-        self.memedImage = memedImage
-    }
 }
